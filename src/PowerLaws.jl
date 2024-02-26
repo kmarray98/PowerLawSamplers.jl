8943d0aa3b5e9120e14b05a5fe1cd99773a066ed
Kieran Marray <77078860+kmarray98@users.noreply.github.com>
2024-02-26 17:46:27 +0100

    Add files via upload

---
+++ src/PowerLaws.jl
@@ -0,0 +1,8 @@
+module PowerLaws
+
+# Write your package code here.
+export double_up_sampler, bounded_double_up_sampler, inexact_sampler, pl_graph
+
+include("Samplers.jl", "RandomGraph.jl")
+
+end
